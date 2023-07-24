3c0a585b67375467762dbc4f10e26fe940310064
axyres <prodesaxy@gmail.com>
2023-07-24 10:56:00 +0700

    Update script

--- quangcar/kubectl_deploy_quangcar.sh
+++ quangcar/kubectl_deploy_quangcar.sh
@@ -1,3 +1,3 @@
 kubectl config set-context --current --namespace=devops-nhattien
-kubectl apply -f /home/axyres/kubernetes/quangcar/s-api/deployment.yaml
-kubectl apply -f /home/axyres/kubernetes/quangcar/s-api/quangcar-api-pv.yaml
+kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/deployment.yaml
+kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/quangcar-api-pv.yaml
