58991b1435fb1e44f73e013731253d4929dd297c
axyres <prodesaxy@gmail.com>
2023-07-24 10:40:40 +0700

    Update

--- script/jenkins_job_build_kubernetes.sh
+++ script/jenkins_job_build_kubernetes.sh
@@ -1,2 +1,2 @@
 sleep 60
-curl -u admin:11cd0378b267f4e826385105bc17c31557 -X POST http://192.168.1.2:8080/jenkins/view/Other/job/Restart-Service/build
+curl -u centos:115c036cfb5e51e1389ad3b780c7c73021 -X POST http://192.168.1.2:8080/jenkins/view/Other/job/Restart-Service/build
---
+++ script/metric_kube_monitor.sh
@@ -0,0 +1,1 @@
+kubectl apply -f /home/axyres/kubernetes/metrics-kube/components.yaml
