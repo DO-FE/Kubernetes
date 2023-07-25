4c48c287d020f8f399c04af6f8a9b9e5679929a7
axyres <prodesaxy@gmail.com>
2023-07-25 12:55:18 +0700

    Update script deploy

--- quangcar/kubectl_deploy_quangcar.sh
+++ quangcar/kubectl_deploy_quangcar.sh
@@ -1,3 +1,7 @@
 kubectl config set-context --current --namespace=devops-nhattien
+
+kubectl delete deployment quangcar-api || true
+sleep 10
+
 kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/deployment.yaml
 kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/quangcar-api-pv.yaml
--- shopmates/kubectl_deploy_shopmates.sh
+++ shopmates/kubectl_deploy_shopmates.sh
@@ -1,4 +1,15 @@
 kubectl config set-context --current --namespace=devops-nhattien
+
+kubectl delete deployment shopmates-api || true
+sleep 10
+
+kubectl delete deployment shopmates-web || true
+sleep 10
+
+kubectl delete deployment shopmates-admin || true
+sleep 10
+
+
 kubectl apply -f /home/axyres/kubernetes/shopmates/s-api/deployment.yaml
 kubectl apply -f /home/axyres/kubernetes/shopmates/s-api/shopamtes-api-pv.yaml
 sleep 10
