87d423f3c0b48ef92b4dc697c0ff25f43a3b88ea
axyres <prodesaxy@gmail.com>
2023-08-07 11:59:20 +0700

    Update deployment quangcar

--- quangcar/kubectl_deploy_quangcar.sh
+++ quangcar/kubectl_deploy_quangcar.sh
@@ -5,7 +5,8 @@ kubectl delete deployment quangcar-admin || true
 sleep 10
 
 kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/deployment.yaml
-kubectl apply -f /home/axyres/kubernetes/quangcar/q-api/quangcar-api-pv.yaml
 
 kubectl apply -f /home/axyres/kubernetes/quangcar/q-admin/deployment.yaml
-kubectl apply -f /home/axyres/kubernetes/quangcar/q-admin/quangcar-admin-pv.yaml
+#kubectl apply -f /home/axyres/kubernetes/quangcar/q-admin/quangcar-admin-pv.yaml
+
+kubectl apply -f /home/axyres/kubernetes/quangcar/quangcar-pv.yaml
